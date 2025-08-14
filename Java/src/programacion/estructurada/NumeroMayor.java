349224190e2b49854048b21acdd8f62a8e6e0baa
Silvia Giardini <110245676+Deekyare@users.noreply.github.com>
2025-08-14 14:39:40 -0300

    Solución al ejercicio: Encontrar el número mayor

---
+++ Java/src/programacion/estructurada/NumeroMayor.java
@@ -0,0 +1,29 @@
+package programacion.estructurada;
+
+/* Determinar el Mayor de Tres Números. Escribe un programa en Java que pida al usuario tres números enteros y determine cuál es el mayor.*/
+
+import java.util.Scanner;
+
+public class NumeroMayor {
+    public static void main(String[] args) {
+        Scanner numIngresado = new Scanner(System.in);
+        int[] numeros = new int[3];
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Ingrese el numero " + (i + 1) + ": ");
+            numeros[i] = numIngresado.nextInt();
+        }
+        int mayor = calcularMayor(numeros);
+        System.out.println("El numero mayor es: " + mayor);
+    }
+    public static int calcularMayor (int[] numeros) {
+        int mayor=numeros[0];
+        for (int i = 1 ; i < numeros.length; i++) {
+            if (numeros[i] > mayor) {
+                mayor = numeros[i];
+            }
+
+            }return mayor;
+        }
+    }
+
