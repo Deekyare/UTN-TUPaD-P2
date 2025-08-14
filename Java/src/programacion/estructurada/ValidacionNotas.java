0b13041a6a86d4aa9db136206e2414251f359e14
Silvia Giardini <110245676+Deekyare@users.noreply.github.com>
2025-08-14 18:55:46 -0300

    Solución del ejercicio: Validación de notas

---
+++ Java/src/programacion/estructurada/ValidacionNotas.java
@@ -0,0 +1,25 @@
+package programacion.estructurada;
+
+/*Validación de Nota entre 0 y 10 (do-while).
+Escribe un programa que solicite al usuario una nota entre 0 y 10. Si el
+usuario ingresa un número fuera de este rango, debe seguir pidiéndole la nota
+hasta que ingrese un valor válido.*/
+
+import java.util.Scanner;
+
+public class ValidacionNotas {
+    public static void main(String[] args) {
+        Scanner notas = new Scanner(System.in);
+        int nota;
+        do {
+            System.out.print("Ingrese una nota entre 0 y 10: ");
+            nota = notas.nextInt();
+            // Si la nota es inválida
+            if (nota < 0 || nota > 10) {
+                System.out.println("Error: Nota inválida. Por favor, intente de nuevo.");
+            }
+        } while (nota < 0 || nota > 10);
+
+        System.out.println("Nota guardada correctamente");
+    }
+}
