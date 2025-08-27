18bcebdd1ec6db2b9f8370f9846b84f89256e8a2
Silvia Giardini <110245676+Deekyare@users.noreply.github.com>
2025-08-27 09:13:46 -0300

    Solución del ejercicio 3 con poo: Encapsulamiento con la Clase Libro

---
+++ Java/src/introduccionPoo/Encapsulamiento/Libro.java
@@ -0,0 +1,43 @@
+package introduccionPoo.Encapsulamiento;
+
+/*
+Crear una clase Libro con atributos privados: titulo, autor,
+añoPublicacion.
+Métodos requeridos: Getters para todos los atributos. Setter con validación
+para añoPublicacion.
+Tarea: Crear un libro, intentar modificar el año con un valor inválido y luego con
+uno válido, mostrar la información final.
+*/
+
+public class Libro {
+    //Atributos privados
+    private String titulo = "Harry Potter";
+    private String autor = "J.K. Rowling";
+    private int anioPublicacion = 1997;
+
+
+    //Métodos
+    //Getters
+    public String getTitulo(){
+        return titulo;
+    }
+    public String getAutor(){
+        return autor;
+    }
+    public int getAnioPublicacion(){
+        return anioPublicacion;
+    }
+
+    //Setter para anioPublicación
+    public void setAnioPublicacion(int anioPublicacion){
+        int anioActual = 2025;
+        if (anioPublicacion >= 1500 && anioPublicacion <= anioActual){
+            this.anioPublicacion = anioPublicacion;
+        }
+
+
+
+    }
+
+
+}
---
+++ Java/src/introduccionPoo/Encapsulamiento/MainLibro.java
@@ -0,0 +1,27 @@
+package introduccionPoo.Encapsulamiento;
+
+/*
+Intentar modificar el año con un valor inválido y luego con
+uno válido, mostrar la información final.
+*/
+
+public class MainLibro {
+    public static void main(String[] args) {
+        Libro libro1 =  new Libro();
+
+        libro1.getTitulo();
+        libro1.getAutor();
+        libro1.getAnioPublicacion();
+
+        System.out.println("--- Datos del libro ---"+ "\nNombre: "+libro1.getTitulo()+ "\nAutor: "+libro1.getAutor()+"\nAño: "+libro1.getAnioPublicacion());
+
+        libro1.setAnioPublicacion(-200);
+
+        System.out.println("--- Datos del libro con valor invalido ---"+ "\nNombre: "+libro1.getTitulo()+ "\nAutor: "+libro1.getAutor()+"\nAño: "+libro1.getAnioPublicacion());
+
+        libro1.setAnioPublicacion(2020);
+
+        System.out.println("--- Datos del libro con valor valido ---"+ "\nNombre: "+libro1.getTitulo()+ "\nAutor: "+libro1.getAutor()+"\nAño: "+libro1.getAnioPublicacion());
+
+    }
+}
